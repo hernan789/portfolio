d91af6627b906cda2837e87326cb2e40b9925659
hernan789 <hernanlduarte@hotmail.com>
2024-05-26 16:01:47 -0300

    update projectdetail

--- portfolio/src/components/ProjectDetail.tsx
+++ portfolio/src/components/ProjectDetail.tsx
@@ -8,15 +8,17 @@ function ProjectDetail() {
   });
   console.log(project);
   return (
-    <section className="bg-black min-h-screen text-white pb-10">
-      <Link to="/">
-        <div className="absolute top-7 min-[120px]:left-4 sm:left-14 md:left-[180px]">
-          <div className=" bg-white w-8 h-8 rounded-full transition-all duration-1000"></div>
-          <h6 className="absolute left-20 top-1 text-white text-xl font-medium hover:underline">
-            Volver
-          </h6>
-        </div>
-      </Link>
+    <section className="bg-black min-h-full  text-white ">
+      <div className="bg-black  h-[100px] pb-[10%]">
+        <Link to="/">
+          <div className="absolute top-7 min-[120px]:left-4 sm:left-14 md:left-[180px]">
+            <div className=" bg-white w-8 h-8 rounded-full transition-all duration-1000"></div>
+            <h6 className="absolute left-20 top-1 text-white text-xl font-medium hover:underline">
+              Volver
+            </h6>
+          </div>
+        </Link>
+      </div>
       <h2 className="proyectos-h2 font-[550] pt-[30%] leading-none sm:tracking-[-2px] md:tracking-[-4px] text-white">
         {project[0].name}
       </h2>
@@ -34,10 +36,64 @@ function ProjectDetail() {
           </h2>
         </div>
       </div>
-      <div className="proyectos-x flex-col justify-items-center ">
-        <img className="mt-8 img-proyect" src={project[0].path} />
+      <div className="proyectos-x pt-[5%] pl-[10%] flex flex-col sm:flex-row gap-14">
+        <img className="mt-8 img-proyect rounded" src={project[0].path6} />
+        <div className="proyectos-texto-derecha font-medium italic pt-[20%]">
+          {project[0].description}{" "}
+        </div>
       </div>
+      <div className="proyectos-x pt-[5%] pl-[10%] flex flex-col sm:flex-row gap-14 pb-[10%]">
+        <div className="proyectos-texto-derecha font-medium italic justify pt-[20%]">
+          {project[0].description}{" "}
+        </div>
+        <img className="mt-8 img-proyect rounded" src={project[0].path5} />
+      </div>
+
+      {/* <div className="max-w-screen flex flex-col items-center justify-center min-h-screen pb-[8%]">
+          <img className="mt-8 img-proyect rounded" src={project[0].path} />
+        </div> */}
       {/* Aquí puedes agregar más detalles del proyecto */}
+      <section className="min-h-full">
+        <div className="grid justify-center pt-[5%]  pb-[5%] bg-yellow-100 ">
+          <img className="mt-8 img-proyect rounded" src={project[0].path6} />
+        </div>
+        {/* <div className="grid justify-items-end pt-[5%]  pr-[10%]  bg-yellow-100  pb-[5%]">
+          <img className="mt-8 img-proyect rounded" src={project[0].path5} />
+        </div> */}
+      </section>
+
+      <div className=" min-h-full align-middle  bg-black flex flex-col text-white text-xl font-medium ">
+        <div className="flex flex-row justify-evenly py-[3%]">
+          <Link to={project[0].link1}>
+            <button className="w-[200px] h-[50px] text-black mt-4 mb-2 p-2 shadow-inner bg-[#ff0] rounded pl-50 flex items-center justify-center hover:bg-white hover:border">
+              Frontend
+            </button>
+          </Link>
+          <Link to={project[0].link2}>
+            <button className="w-[200px] h-[50px] text-black mt-4 mb-2 p-2 shadow-inner bg-[#ff0] rounded pl-50 flex items-center justify-center hover:bg-white hover:border">
+              Backend
+            </button>
+          </Link>
+        </div>
+        <footer className="flex flex-col justify-between mt-4 text-medium text-xl border-t-[1px] border-t-white pb-[5%]">
+          <ul className="flex flex-row justify-evenly mt-12  ">
+            <li className="hover:underline">
+              <a href="https://linkedin.com/in/hernán-duarte-b70a90291">
+                LinkedIn
+              </a>
+            </li>
+            <br />
+            <li className="hover:underline">
+              <a href="https://github.com/hernan789">GitHub</a>
+            </li>
+          </ul>
+          <ul className="mt-10 text-base text-center b-l-[1px] border-l-black ">
+            <li>Copyright © 2024 Hernán Duarte. All Rights Reserved</li>
+          </ul>
+
+          <div></div>
+        </footer>
+      </div>
     </section>
   );
 }
